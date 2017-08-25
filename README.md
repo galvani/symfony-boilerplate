c712ebf8cd9bf10265f260879d07642c052aaff3
Jan Kozak <galvani78@gmail.com>
2017-08-25 19:58:41 +0200

    dotenv, flex, symfony 3.3, no bundles.php yet

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony-boilerplate
+===================
+
+A Symfony project created on August 25, 2017, 4:24 pm.
---
+++ app/AppKernel.php
@@ -0,0 +1,63 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+	public function __construct() {
+		(new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__.'/../.env');
+
+		if (!getenv('ENV')) {
+			throw new \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException('Invalid environment');
+		}
+		parent::__construct(getenv('ENV'), getenv('DEBUG'));
+	}
+
+	public function registerBundles()
+    {
+        $bundles = [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AppBundle\AppBundle(),
+        ];
+
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+
+            if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+            }
+        }
+
+        return $bundles;
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/../config/'.$this->getEnvironment().'/config.yml');
+    }
+}
